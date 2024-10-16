93a66143783286cab28fd89c39f80374bcc4aa14
aygalic <aygalic@hotmail.fr>
2024-10-16 12:09:28 +0200

    chore: refactor API and delete legacy routes

--- app/run_quantized.py
+++ app/run_quantized.py
@@ -16,26 +16,11 @@ class ChatHistory(BaseModel):
     item: list[tuple[str, str]]
 
 
-@app.post("/llm_on_cpu")
-def legacy_summary(query: Query) -> str:
-    """Legacy function to ask for a summary without using token streaming.
-
-    Parameters
-    ----------
-    query : Query
-        Article to summarize
-
-    Returns
-    -------
-    str
-        Summary provided by the LLM
-    """
-    return chat.request_summary(query.item)
-
-
-@app.post("/llm_on_cpu_stream")
+@app.post("/llm_on_cpu_stream") # legacy route
+@app.post("/summarize")
 async def stream_summary(query: Query) -> StreamingResponse:
-    """Request summary and stream the response
+    """Request summary and stream the response.
+    Uses a non persistent chat object with specific prompt for this.
 
     Parameters
     ----------
@@ -49,30 +34,35 @@ async def stream_summary(query: Query) -> StreamingResponse:
 
     """
     async def generate():
-        for token in chat.request_summary_stream(query.item):
-            yield f"data: {token}\n\n"
+        summarizer = Chat(
+            "Your aim is to summarize and article in the most concise way possible")
 
-    return StreamingResponse(generate(), media_type="text/event-stream")
-
-
-@app.post("/stream_misc_query")
-async def stream_query(query: Query):
-    async def generate():
-        for token in chat.single_query_stream(query.item):
+        for token in summarizer.generate_summary(query.item):
             yield f"data: {token}\n\n"
 
     return StreamingResponse(generate(), media_type="text/event-stream")
 
 
 @app.post("/stream_chat")
-async def stream_chat(hist: ChatHistory):
+async def stream_chat(hist: ChatHistory) -> StreamingResponse:
+    """Simple chatbot
+
+    Parameters
+    ----------
+    hist : ChatHistory
+        Chat history that the AI will use to generate its next reply.
+
+    Returns
+    -------
+    StreamingResponse
+        Streaming response provided by the LLM.
+    """
+
     async def generate():
-        for token in chat.stream_chat(hist.item):
+        for token in chat.generate_chat_response(hist.item):
             yield f"data: {token}\n\n"
 
     return StreamingResponse(generate(), media_type="text/event-stream")
-
-
 
 
 
--- llama_summarizer/chat.py
+++ llama_summarizer/chat.py
@@ -3,43 +3,30 @@ from . import MODELS_DIR
 MODEL_PATH = str(MODELS_DIR / "Q4_K_M.gguf")
 
 class Chat():
-    def __init__(self):
+    def __init__(self, init_prompt : str | None = None):
         self.llm = Llama(model_path=MODEL_PATH, n_ctx=2048, n_threads=4, verbose=False)
         self.max_tokens = 500
+        self.prompt_history : list[dict[str, str]] = []
 
-    def request_summary(self, message: str) -> str:
-        messages = [
-            {
-                "role": "system",
-                "content": "You aim is to summarize and article in the most concise way possible."
-            },
-            {
-                "role": "user",
-                "content": message
-            },
-        ]
-        return self._generate_response(messages)
+        self.prompt_history += [{
+            "role": "system",
+            "content":[init_prompt, "You are an AI assistant that responds to user requests."][init_prompt is None]
+        }]
 
-    def request_summary_stream(self, message: str):
-        messages = [
-            {
-                "role": "system",
-                "content": "You aim is to summarize and article in the most concise way possible."
-            },
-            {
-                "role": "user",
-                "content": message
-            },
-        ]
-        yield from self._generate_response_stream(messages)
+    def generate_summary(self, message: str):
+        """Generate Summary for the user.
 
+        Parameters
+        ----------
+        message : str
+            Article tu summarize
 
-    def single_query_stream(self, message: str):
-        messages = [
-            {
-                "role": "system",
-                "content": "You are an AI assistant that responds to user requests and is very concise in its answers."
-            },
+        Yields
+        ------
+        str
+            Summary
+        """
+        messages = self.prompt_history + [
             {
                 "role": "user",
                 "content": message
@@ -48,15 +35,20 @@ class Chat():
         yield from self._generate_response_stream(messages)
 
 
+    def generate_chat_response(self, messages: list[tuple[str, str]]):
+        """Generate chat response
 
-    def stream_chat(self, messages: list[tuple[str, str]]):
+        Parameters
+        ----------
+        messages : list[tuple[str, str]]
+            Chat history
 
-        prompt = [
-            {
-                "role": "system",
-                "content": "You are an AI assistant that responds to user requests and is very concise in its answers."
-            }
-        ]
+        Yields
+        ------
+        str
+            new LLM response.
+        """
+        prompt = self.prompt_history
         for role, content in messages:
             prompt.append(
                 {
@@ -65,21 +57,39 @@ class Chat():
                 }
             )
 
+        yield from self._generate_response_stream(prompt)
 
-
-        yield from self._generate_response_stream(prompt)    
-
-    def _generate_response(self, query: list) -> str:
-        prompt = self._create_prompt(query)
-        output = self.llm(prompt, max_tokens=self.max_tokens, stop=["user:", "system:", "\n"])
-        return output['choices'][0]['text'].strip()
 
     def _generate_response_stream(self, query: list):
+        """Streaming interface with the model.
+
+        Parameters
+        ----------
+        query : list
+            List of messages
+
+        Yields
+        ------
+        str
+            LLM response
+        """
         prompt = self._create_prompt(query)
         for output in self.llm(prompt, max_tokens=self.max_tokens, stop=["user:", "system:", "\n"], stream=True):
             yield output['choices'][0]['text']
 
     def _create_prompt(self, query: list) -> str:
+        """Create a valid prompt
+
+        Parameters
+        ----------
+        query : list
+            List of messages
+
+        Returns
+        -------
+        str
+            Formatted prompt
+        """
         prompt = ""
         for message in query:
             role = message["role"]
