0b36553bc04ca24ae9f8835cc1c3c65bf9f62e85
aygalic <aygalic@hotmail.fr>
2024-11-22 16:07:00 +0100

    chore: typing

--- llama_summarizer/random_article.py
+++ llama_summarizer/random_article.py
@@ -1,24 +1,24 @@
 import requests
-import json
 
-def get_random_wikipedia_article():
+
+def get_random_wikipedia_article() -> tuple[str, str]:
     # API endpoint for a random article
     url = "https://en.wikipedia.org/w/api.php"
-    
+
     # Parameters for the API request
     params = {
         "action": "query",
         "format": "json",
         "list": "random",
         "rnlimit": "1",
-        "rnnamespace": "0"
+        "rnnamespace": "0",
     }
-    
+
     # Get the random article title
     response = requests.get(url, params=params)
     data = response.json()
     title = data["query"]["random"][0]["title"]
-    
+
     # Parameters for getting the full content
     params = {
         "action": "query",
@@ -26,14 +26,13 @@ def get_random_wikipedia_article():
         "titles": title,
         "prop": "extracts",
         "explaintext": True,
-        "exsectionformat": "plain"
+        "exsectionformat": "plain",
     }
-    
+
     # Get the full content
     response = requests.get(url, params=params)
     data = response.json()
     page = next(iter(data["query"]["pages"].values()))
     content = page["extract"]
-    
-    return title, content
 
+    return title, content
