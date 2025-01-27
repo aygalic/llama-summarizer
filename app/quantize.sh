7f0916a096b9bef4b429aec7585760a142cc8669
aygalic <aygalic@hotmail.fr>
2025-01-27 15:39:25 +0100

    chore: use (mostly) relative paths

--- app/quantize.sh
+++ app/quantize.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 mkdir -p ./models
 
 #Download model
@@ -8,6 +9,6 @@ huggingface-cli download "meta-llama/Llama-3.2-1B-Instruct" --local-dir "./model
 python /Users/aygalic/github/llama.cpp/convert_hf_to_gguf.py ./models
 
 #Quantize and save model
-llama-quantize /Users/aygalic/github/llama-summarizer/models/Models-1.2B-F16.gguf /Users/aygalic/github/llama-summarizer/models/Q4_K_M.gguf Q4_K_M
+llama-quantize ./models/Models-1.2B-F16.gguf ./models/Q4_K_M.gguf Q4_K_M
 
 
